29a561116c54642f755ad2b9db385d5b2c13b2f1
Jaquedmonteiro <jaquem.rs@hotmail.com>
2022-02-22 14:58:43 -0300

    atividade crud

--- src/main/java/org/generation/blogpessoal/model/Postagens.java
+++ src/main/java/org/generation/blogpessoal/model/Postagens.java
@@ -6,12 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table (name = "postagens")
@@ -31,6 +33,10 @@ public class Postagens {
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagens")
+	private Tema tema;
 	
 	public long getId() {
 		return id;
