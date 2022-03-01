a7543a16e57099a23c31efeceada8dbef3f835be
Jaque Domingues <jaquem.rs@hotmail.com>
2022-03-01 19:58:39 -0300

    add documentation

--- src/main/java/org/generation/blogpessoal/BlogpessoalApplication.java
+++ src/main/java/org/generation/blogpessoal/BlogpessoalApplication.java
@@ -3,11 +3,15 @@ package org.generation.blogpessoal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+/*
+Classe inicial do aplicativo Spring Boot
+*/
+
+@SpringBootApplication /*anotação que lê, configura automaticamente todos os frameworks e executa o projeto.*/
 public class BlogpessoalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogpessoalApplication.class, args);
+		SpringApplication.run(BlogpessoalApplication.class, args);/*Linha principal que roda o projeto.*/
 	}
 
 }
